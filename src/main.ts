1cb3108c08c68873ed084993565ab1c9bf890c1a
Marcus Tillmanns <marcus.tillmanns@qt.io>
2024-10-25 15:09:31 +0200

    Initial test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+
+import axios from 'axios'
+import FormData from 'form-data'
+import fs from 'fs'
 
 /**
  * The main function for the action.
@@ -7,18 +10,59 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const mac_in: string = core.getInput('macos')
+    const win_x64_in: string = core.getInput('win-x64')
+    const win_arm64_in: string = core.getInput('win-arm64')
+    const jenkinsUrl: string = core.getInput('jenkins-url')
+    const jenkinsUser: string = core.getInput('jenkins-user')
+    const jenkinsToken: string = core.getInput('jenkins-token')
+
+    const triggerUrl = `${jenkinsUrl}/job/Sign_archive/build`
+
+    const inputFiles = [
+      { name: 'input_mac_7z', file: mac_in, paramName: 'macos' },
+      { name: 'input_windows_x64_7z', file: win_x64_in, paramName: 'winx64' },
+      {
+        name: 'input_windows_arm64_7z',
+        file: win_arm64_in,
+        paramName: 'winarm64'
+      }
+    ].filter(file => file.file)
+
+    const form = new FormData()
+    const params = {
+      parameters: inputFiles.map(file => {
+        return { name: file.name, file: file.paramName }
+      })
+    }
+
+    inputFiles.forEach(file => {
+      form.append(file.paramName, fs.createReadStream(file.file))
+    })
+    form.append('json', JSON.stringify(params))
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Sending request ...`)
+    core.debug(`Parameters: ${JSON.stringify(params)}`)
+    core.debug(`Files: ${inputFiles.map(file => file.file).join(', ')}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const config = {
+      method: 'post',
+      url: triggerUrl,
+      headers: {
+        Authorization: `Basic ${Buffer.from(`${jenkinsUser}:${jenkinsToken}`).toString('base64')}`,
+        ...form.getHeaders()
+      },
+      data: form
+    }
+
+    const triggerResult = await axios(config)
+    core.debug(`Response: ${JSON.stringify(triggerResult.data)}`)
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('macos', '')
+    core.setOutput('win-x64', '')
+    core.setOutput('win-arm64', '')
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
